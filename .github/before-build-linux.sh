50f061d7df6624f5568c40fe9fc2405eab8eb433
itsusinn <itsusinn@foxmail.com>
2021-10-17 11:11:46 +0800

    ci: add g++ for linux-musl

--- .github/before-build-linux.sh
+++ .github/before-build-linux.sh
@@ -1,5 +1,6 @@
-sudo apt install -y musl-tools
+sudo apt install -y musl-tools clang
 sudo ln -s /usr/include/x86_64-linux-gnu/asm /usr/include/x86_64-linux-musl/asm
 sudo ln -s /usr/include/asm-generic /usr/include/x86_64-linux-musl/asm-generic
 sudo ln -s /usr/include/linux /usr/include/x86_64-linux-musl/linux
+sudo ln -s /usr/bin/g++ /usr/bin/musl-g++
 rustup target add x86_64-unknown-linux-musl
