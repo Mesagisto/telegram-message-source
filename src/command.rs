08647aaedf6744fd88d066bb5052fd784bf280df
itsusinn <itsusinn@foxmail.com>
2021-10-17 11:22:26 +0800

    style: rust fmt

--- src/command.rs
+++ src/command.rs
@@ -1,43 +1,44 @@
 use crate::config::CONFIG;
 use std::sync::Arc;
+use arcstr::ArcStr;
 use teloxide::dispatching::UpdateWithCx;
 use teloxide::prelude::*;
-use teloxide::requests::ResponseResult;
 use teloxide::utils::command::BotCommand;
 
 #[derive(BotCommand)]
 #[command(rename = "lowercase", description = "信使Bot支持以下命令")]
 pub enum Command {
-    #[command(description = "显示命令帮助")]
-    Help,
-    #[command(description = "启用消息转发")]
-    Enable,
-    #[command(description = "禁用消息转发")]
-    Disable,
-    #[command(description = "设置当前Group的转发地址", parse_with = "split")]
-    SetAddress { address: String },
+  #[command(description = "显示命令帮助")]
+  Help,
+  #[command(description = "启用消息转发")]
+  Enable,
+  #[command(description = "禁用消息转发")]
+  Disable,
+  #[command(description = "设置当前Group的转发地址", parse_with = "split")]
+  SetAddress { address: String },
 }
 
 pub async fn answer(
-    cx: Arc<UpdateWithCx<AutoSend<Bot>, Message>>,
-    command: Command,
-) -> ResponseResult<()> {
-    match command {
-        Command::Help => {
-            cx.answer(Command::descriptions()).await?;
-        }
-        Command::Enable => {
-            cx.answer("Mesagisto信使已启用").await?;
-        }
-        Command::Disable => {
-            cx.answer("Mesagisto信使已禁用").await?;
-        }
-        Command::SetAddress { address } => {
-            CONFIG
-                .target_address_mapper
-                .insert(Arc::new(cx.chat_id().to_string()), Arc::new(address));
-            cx.answer("成功设置当前Group的信使地址").await?;
-        }
-    };
-    Ok(())
+  cx: Arc<UpdateWithCx<AutoSend<Bot>, Message>>,
+  command: Command,
+) -> anyhow::Result<()> {
+  match command {
+    Command::Help => {
+      cx.answer(Command::descriptions()).await?;
+    }
+    Command::Enable => {
+      cx.answer("Mesagisto信使已启用").await?;
+    }
+    Command::Disable => {
+      cx.answer("Mesagisto信使已禁用").await?;
+    }
+    Command::SetAddress { address } => {
+      // fixme check permission
+      CONFIG
+        .target_address_mapper
+        .insert(cx.chat_id(), ArcStr::from(address));
+      cx.answer("成功设置当前Group的信使地址").await?;
+    }
+  };
+  Ok(())
 }
--- src/despatch.rs
+++ src/despatch.rs
@@ -1,59 +1,55 @@
+use crate::config::CONFIG;
 use crate::webhook::webhook;
 use std::fmt::Debug;
 use std::future::Future;
 use std::sync::Arc;
 use teloxide::dispatching::update_listeners::UpdateListener;
-use teloxide::dispatching::{Dispatcher, DispatcherHandlerRx, UpdateWithCx, update_listeners};
-use teloxide::error_handlers::OnError;
+use teloxide::dispatching::{update_listeners, Dispatcher, DispatcherHandlerRx, UpdateWithCx};
+use teloxide::error_handlers::{ErrorHandler, OnError};
 use teloxide::prelude::*;
-use teloxide::{
-    utils::command::BotCommand,
-};
+use teloxide::utils::command::BotCommand;
 use teloxide_core::types::Message;
 use tokio_stream::wrappers::UnboundedReceiverStream;
-use crate::config::CONFIG;
 
-pub async fn cmd_or_msg_repl<N,Cmd, CH, MH, FutC, FutM, ErrC, ErrM>(
-    bot: AutoSend<Bot>,
-    bot_name: N,
-    cmd_handler: CH,
-    msg_handler: MH,
+pub async fn cmd_or_msg_repl<N, Cmd, CH, MH, FutC, FutM>(
+  bot: &AutoSend<Bot>,
+  bot_name: N,
+  cmd_handler: CH,
+  msg_handler: MH,
 ) where
-    Cmd: BotCommand + Send + 'static,
+  Cmd: BotCommand + Send + 'static,
 
-    CH: Fn(Arc<UpdateWithCx<AutoSend<Bot>, Message>>, Cmd) -> FutC + Send + Sync + 'static,
-    FutC: Future<Output = Result<(), ErrC>> + Send + 'static,
-    Result<(), ErrC>: OnError<ErrC>,
-    ErrC: Debug + Send,
+  CH: Fn(Arc<UpdateWithCx<AutoSend<Bot>, Message>>, Cmd) -> FutC + Send + Sync + 'static,
+  FutC: Future<Output = anyhow::Result<()>> + Send + 'static,
 
-    MH: Fn(Arc<UpdateWithCx<AutoSend<Bot>, Message>>, String) -> FutM + Send + Sync + 'static,
-    FutM: Future<Output = Result<(), ErrM>> + Send + 'static,
-    Result<(), ErrM>: OnError<ErrM>,
-    ErrM: Debug + Send,
+  MH: Fn(Arc<UpdateWithCx<AutoSend<Bot>, Message>>) -> FutM + Send + Sync + 'static,
+  FutM: Future<Output = anyhow::Result<()>> + Send + 'static,
 
-    N: Into<String> + Send +Clone + 'static
+  N: Into<String> + Send + Clone + 'static,
 {
-
-   if CONFIG.telegram.webhook.enable {
-        let listener =  webhook(&bot).await;
-        cmd_or_msg_repl_with_listener(bot, bot_name, cmd_handler, msg_handler, listener).await;
-    } else {
-        bot.delete_webhook().await.expect("Failed to delete previous webhook");
-        let listener =  update_listeners::polling_default(bot.clone()).await;
-        cmd_or_msg_repl_with_listener(bot, bot_name, cmd_handler, msg_handler, listener).await;
-    };
+  if CONFIG.telegram.webhook.enable {
+    let listener = webhook(&bot).await;
+    cmd_or_msg_repl_with_listener(&bot, bot_name, cmd_handler, msg_handler, listener).await;
+  } else {
+    bot
+      .delete_webhook()
+      .await
+      .expect("Failed to delete previous webhook");
+    let listener = update_listeners::polling_default(bot.clone()).await;
+    cmd_or_msg_repl_with_listener(&bot, bot_name, cmd_handler, msg_handler, listener).await;
+  };
 }
 
-pub async fn cmd_or_msg_repl_with_listener<'a,N,Cmd, CH, MH, FutC, FutM, ErrC, ErrM,L,ListenerE>(
-    bot: AutoSend<Bot>,
-    bot_name: N,
-    cmd_handler: CH,
-    msg_handler: MH,
-    listener: L,
+pub async fn cmd_or_msg_repl_with_listener<N, Cmd, CH, MH, FutC, FutM, L, ListenerE>(
+  bot: &AutoSend<Bot>,
+  bot_name: N,
+  cmd_handler: CH,
+  msg_handler: MH,
+  listener: L,
 ) where
-    Cmd: BotCommand + Send + 'static,
+  Cmd: BotCommand + Send + 'static,
 
-    CH: Fn(Arc<UpdateWithCx<AutoSend<Bot>, Message>>, Cmd) -> FutC + Send + Sync + 'static,
+  CH: Fn(Arc<UpdateWithCx<AutoSend<Bot>, Message>>, Cmd) -> FutC + Send + Sync + 'static,
     FutC: Future<Output = Result<(), ErrC>> + Send + 'static,
     Result<(), ErrC>: OnError<ErrC>,
     ErrC: Debug + Send,
@@ -63,38 +59,58 @@ pub async fn cmd_or_msg_repl_with_listener<'a,N,Cmd, CH, MH, FutC, FutM, ErrC, E
     Result<(), ErrM>: OnError<ErrM>,
     ErrM: Debug + Send,
 
-    N: Into<String> + Send +Clone + 'static,
+  N: Into<String> + Send + Clone + 'static,
 
-    L: UpdateListener<ListenerE> + Send + 'a,
-    ListenerE: Debug + Send + 'a
+  L: UpdateListener<ListenerE> + Send,
+  ListenerE: Debug + Send,
 {
-    let cmd_handler = Arc::new(cmd_handler);
-    let msg_handler = Arc::new(msg_handler);
+  let cmd_handler = Arc::new(cmd_handler);
+  let msg_handler = Arc::new(msg_handler);
 
-    Dispatcher::new(bot.clone())
-    .messages_handler(
-        move |rx: DispatcherHandlerRx<AutoSend<Bot>, Message>| {
-            UnboundedReceiverStream::new(rx).for_each_concurrent(None, move |cx| {
-                let msg_handler = Arc::clone(&msg_handler);
-                let cmd_handler = Arc::clone(&cmd_handler);
-                let clone_bot_name = bot_name.clone();
-                let cx = Arc::new(cx);
-                async move {
-                    if let Some(text_content) = cx.clone().update.text() {
-                        let parse =  Cmd::parse(&*text_content, clone_bot_name);
-                        match parse{
-                            Ok(cmd) => cmd_handler(cx.to_owned(), cmd).await.log_on_error().await,
-                            Err(_) => msg_handler(cx.to_owned(), String::from(text_content)).await.log_on_error().await
-                        }
-                    };
-                }
-            })
-        },
-    )
+  Dispatcher::new(bot.clone())
+    .messages_handler(move |rx: DispatcherHandlerRx<AutoSend<Bot>, Message>| {
+      UnboundedReceiverStream::new(rx).for_each_concurrent(None, move |cx| {
+        let msg_handler = Arc::clone(&msg_handler);
+        let cmd_handler = Arc::clone(&cmd_handler);
+        let clone_bot_name = bot_name.clone();
+        let cx = Arc::new(cx);
+        async move {
+          if let Some(text_content) = cx.clone().update.text() {
+            let parse = Cmd::parse(&*text_content, clone_bot_name);
+            match parse {
+              Ok(cmd) => cmd_handler(cx.to_owned(), cmd).await.log_on_error().await,
+              Err(_) => msg_handler(cx).await.log_on_error().await,
+            }
+          } else {
+            msg_handler(cx).await.log_on_error().await
+          };
+        }
+      })
+    })
     .setup_ctrlc_handler()
     .dispatch_with_listener(
-        listener,
-        LoggingErrorHandler::with_custom_text("An error from the update listener"),
+      listener,
+      LoggingErrorHandler::with_custom_text("An error from the update listener"),
     )
     .await
+}
+
+struct TracingErrorHandler {
+  text: String,
+}
+impl TracingErrorHandler {
+  #[allow(unused)]
+  pub fn with_custom_text<T>(text: T) -> Arc<Self>
+  where
+    T: Into<String>,
+  {
+    Arc::new(Self { text: text.into() })
+  }
+}
+use futures::future::BoxFuture;
+impl ErrorHandler<anyhow::Error> for TracingErrorHandler {
+  fn handle_error(self: Arc<Self>, error: anyhow::Error) -> BoxFuture<'static, ()> {
+    log::error!("{}:{}, \n Backtrace {}",self.text,error,error.backtrace());
+    Box::pin(async {})
+  }
 }
