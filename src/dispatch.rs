d442f8fd0ead215bcf86ed85e7fc2edd6411faf2
itsusinn <itsusinn@foxmail.com>
2022-06-20 16:58:40 +0800

    feat: supporting channel post and edited message, step 1
    add event listener

--- src/dispatch.rs
+++ src/dispatch.rs
@@ -1,20 +1,32 @@
 use teloxide::prelude::*;
 use tracing::info;
 
-use crate::{command::Command, message::handlers};
 use crate::bot::BotRequester;
+use crate::{command::Command, message::handlers};
 
 pub async fn start(bot: &BotRequester) {
-  let handler = Update::filter_message()
+  let message_handler = Update::filter_message()
     .branch(
       dptree::entry()
         .filter_command::<Command>()
         .endpoint(Command::answer),
     )
     .branch(
-      dptree::filter(|msg: Message| msg.chat.is_group() || msg.chat.is_supergroup())
-        .endpoint(handlers::send::answer_common),
+      dptree::filter(|msg: Message| {
+        msg.chat.is_group() || msg.chat.is_supergroup() || msg.chat.is_channel()
+      })
+      .endpoint(handlers::send::answer_common),
     );
+  let edit_message_handler = Update::filter_edited_message().branch(
+    dptree::filter(|msg: Message| {
+      msg.chat.is_group() || msg.chat.is_supergroup() || msg.chat.is_channel()
+    })
+    .endpoint(handlers::send::answer_common),
+  );
+  let handler = dptree::entry()
+    .branch(message_handler)
+    .branch(edit_message_handler);
+
   info!("Mesagisto信使启动成功");
   Dispatcher::builder(bot.clone(), handler)
     .error_handler(LoggingErrorHandler::with_custom_text(
