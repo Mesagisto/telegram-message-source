2610df3730fb0e1c963f2ac850ac9fbc7f9cf380
Itsusinn <itsusinn@meowcat.org>
2022-11-05 17:11:30 +0800

    refactor: remove polylith

--- src/log.rs
+++ src/log.rs
@@ -5,18 +5,14 @@ use tracing_error::ErrorLayer;
 use tracing_subscriber::{layer::SubscriberExt, prelude::*};
 
 pub(crate) async fn init() -> Result<()> {
-  #[cfg(feature = "polylith")]
-  opentelemetry::global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
-
-  // TODO EnvFilter;
-  #[cfg(any(feature = "polylith", debug_assertions))]
+  #[cfg(debug_assertions)]
   let filter = tracing_subscriber::filter::Targets::new()
-    .with_target("telegram_message_source", Level::TRACE)
+    .with_target("msgist_tg", Level::TRACE)
     .with_target("mesagisto_client", Level::TRACE)
     .with_target("msgist", Level::TRACE)
     .with_target("teloxide", Level::TRACE)
     .with_default(Level::INFO);
-  #[cfg(not(any(feature = "polylith", debug_assertions)))]
+  #[cfg(not(debug_assertions))]
   let filter = tracing_subscriber::filter::Targets::new()
     .with_target("msgist_tg", Level::DEBUG)
     .with_target("mesagisto_client", Level::DEBUG)
@@ -24,15 +20,6 @@ pub(crate) async fn init() -> Result<()> {
     .with_default(Level::WARN);
 
   let registry = tracing_subscriber::registry();
-  #[cfg(feature = "polylith")]
-  let tracer = opentelemetry_jaeger::new_pipeline()
-    .with_service_name("telegram-source")
-    .install_simple()?;
-  // Create a tracing layer with the configured tracer
-  #[cfg(feature = "polylith")]
-  let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
-  #[cfg(feature = "polylith")]
-  let registry = registry.with(telemetry);
   registry
     .with(filter)
     .with(ErrorLayer::default())
